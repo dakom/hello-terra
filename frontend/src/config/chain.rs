d76c275067a33f6248d03b12f38d6d747ddbf6b8
David Komer <david.komer@gmail.com>
2021-09-20 12:12:34 +0300

    log bombay values

--- frontend/src/config/chain.rs
+++ frontend/src/config/chain.rs
@@ -1,2 +1,6 @@
 pub const DEFAULT_LOCAL_TERRA_HOST:&'static str = "http://localhost:3060";
 pub const DEFAULT_LOCAL_TERRA_CHAIN:&'static str = "localterra";
+
+
+//pub const DEFAULT_LOCAL_TERRA_HOST:&'static str = "https://bombay-lcd.terra.dev";
+//pub const DEFAULT_LOCAL_TERRA_CHAIN:&'static str = "bombay-10";
